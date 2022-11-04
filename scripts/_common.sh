bfbb69e20214ba239dbc08d43237266dd4c7401a
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-11-04 22:32:58 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,8 +6,6 @@
 
 YNH_PHP_VERSION="8.1"
 
-#REMOVEME? pkg_dependencies="ffmpeg php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mysqlnd"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
