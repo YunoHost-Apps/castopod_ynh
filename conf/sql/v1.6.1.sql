9b7764aaee44c65c7191232146a35d6f62ea3869
Mateusz <2871798+orhtej2@users.noreply.github.com>
2023-10-30 11:57:39 +0100

    Update v1.6.1.sql

--- conf/sql/v1.6.1.sql
+++ conf/sql/v1.6.1.sql
@@ -1,2 +1,2 @@
-INSERT INTO `cp_platforms` (`slug`, `type`, `label`, `home_url`, `submit_url`) VALUES
+INSERT IGNORE INTO `cp_platforms` (`slug`, `type`, `label`, `home_url`, `submit_url`) VALUES
 ('tiktok', 'social', 'Tiktok', 'https://www.tiktok.com/', 'https://www.tiktok.com/signup');
