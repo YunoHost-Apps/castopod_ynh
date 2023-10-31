b6634f3a3ba3246710f0bdf6d3db82624d5130f2
Mateusz <2871798+orhtej2@users.noreply.github.com>
2023-10-31 20:38:00 +0100

    Version 1.6.5 take 2 (#109)
    * Fix install and upgrade behaviour.
    
    * Properly seed database.
    
    * Fixup $data_dir permissions.
    
    * Serve contents of media directory.
    
    * Fully replace sources during upgrade.
    
    * Fixed typo.
    
    * Run more migrations.
    
    * More escaping?
    
    * Fix paths.
    
    * Fix usage of mv
    
    * Update install
    
    * Update install
    
    * Update upgrade
    
    * Update v1.5.2.sql
    
    * Update v1.6.1.sql
    
    * Update upgrade
    
    * Allow login with credentials from old versions.
    
    * Rename a file as it should be named
    
    * Update paths in patch.

---
+++ conf/sql/v1.6.1.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `cp_platforms` (`slug`, `type`, `label`, `home_url`, `submit_url`) VALUES
+('tiktok', 'social', 'Tiktok', 'https://www.tiktok.com/', 'https://www.tiktok.com/signup');
