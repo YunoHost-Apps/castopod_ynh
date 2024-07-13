fb90a39e339e7c63ebf905755651afa6e12f8f25
Mateusz <2871798+orhtej2@users.noreply.github.com>
2024-07-13 23:47:18 +0200

    Fix migrations as `pc_platforms` changed.

--- conf/sql/v1.11.0.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `cp_platforms` (`slug`, `type`, `label`, `home_url`, `submit_url`) VALUES
-('telegram', 'social', 'Telegram', 'https://www.telegram.org/', ''),
-('episodes-fm', 'podcasting', 'Episodes.fm', 'https://episodes.fm/', 'https://podcastindex.org/add'),
-('truefans', 'podcasting', 'Truefans', 'https://truefans.fm/', 'https://podcastindex.org/add');
